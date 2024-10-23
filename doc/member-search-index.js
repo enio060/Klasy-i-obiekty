95f03ec3246a95fb8ee7d84f43d2470133ee6934
Kacper Biłka <kacperbilka1@gmail.com>
2024-10-23 13:53:57 +0200

    Commit2

---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Program","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Program","l":"Program()","u":"%3Cinit%3E()"}];updateSearchResults();
